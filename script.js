16e16618e6b78647d338c6182c6fc3d4245509ef
Janhavi3045 <janhavi3045@gmail.com>
2026-07-16 23:28:20 +0530

    Add attendance marking functionality

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const attendanceList = document.getElementById("attendanceList");
 
+function markAttendance(status){
+
+    const name = document.getElementById("studentName").value.trim();
+
+    if(name===""){
+        alert("Please enter student name.");
+        return;
+    }
+
+    const row = document.createElement("tr");
+
+    const now = new Date();
+
+    row.innerHTML = `
+        <td>${name}</td>
+        <td>${status}</td>
+        <td>${now.toLocaleString()}</td>
+    `;
+
+    attendanceList.appendChild(row);
+
+    document.getElementById("studentName").value="";
+}
